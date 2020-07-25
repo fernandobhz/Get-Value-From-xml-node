23b80a145963737b8806d6e350ab90197d9c430b
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:05:02 -0300

    old project migrated

---
+++ Get Value From xml node/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+
+        Dim s As String = Me.TextBox1.Text
+        s = s.Substring(s.LastIndexOf("<trigger_error>") + Len("<trigger_error>"), s.LastIndexOf("</trigger_error>") - s.LastIndexOf("<trigger_error>") - Len("<trigger_error>"))
+        
+
+
+        MsgBox(s)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        MsgBox(Me.TextBox1.Text.LastIndexOf("banana") = False)
+    End Sub
+End Class
